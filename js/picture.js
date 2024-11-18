ff60b806c16c5041a7c89f9a1b2b04e0aac8ea7c
Алиса Егорова <girlaliceee@yandex.ru>
2024-11-18 14:27:56 +0500

    открывается и закрывается

--- js/picture.js
+++ js/picture.js
@@ -1,5 +1,5 @@
-
 import { createPhotoDescriptions } from './data.js';
+import { showBigPicture } from './big-picture.js';
 
 const picturesList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture');
@@ -10,8 +10,18 @@ function createPictureElement(photoData) {
   newPicture.querySelector('.picture__img').alt = photoData.description;
   newPicture.querySelector('.picture__comments').textContent = photoData.comments.length;
   newPicture.querySelector('.picture__likes').textContent = photoData.likes;
+
+  const onPictureElementClick = (evt) => {
+    evt.preventDefault();
+
+    showBigPicture(photoData);
+  };
+
+  newPicture.addEventListener('click', onPictureElementClick);
+
   return newPicture;
 }
+
 function renderPictures(photoData) {
   const fragment = new DocumentFragment();
   photoData.forEach((photo) => {
